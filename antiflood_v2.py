027019f98c332e94b28065052dbe6e2d6658d188
Davide Galilei <43778739+DavideGalilei@users.noreply.github.com>
2020-06-07 20:28:10 +0200

    V2
    The second version, improved by github.com/nocturn9x.

---
+++ antiflood_v2.py
@@ -0,0 +1,28 @@
+from time import time
+from collections import defaultdict
+from typing import Union
+
+USERS = defaultdict(list)
+MESSAGES = 3
+SECONDS = 3
+
+
+def is_flood(uid: int) -> Union[bool, None]:
+    """Checks if a user is flooding"""
+
+    USERS[uid].append(time())
+    if len(list(filter(lambda x: time() - int(x) < SECONDS, USERS[uid]))) > MESSAGES:
+        return True
+
+
+if __name__ == "__main__":
+    while 1:
+        try:
+            input(">>> ")
+        except (EOFError, KeyboardInterrupt):
+            break
+        if is_flood(12345678):
+            print("flood")
+        else:
+           print("non flood")
+    print()
